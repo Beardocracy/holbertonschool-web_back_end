127c344eaf89e73cb3d72236e779b8a5de767798
Beardocracy <travisjbearden@gmail.com>
2021-03-15 13:23:47 -0500

    Added part 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Iterable, Sequence, List, Tuple
+'''
+This module contains a type-annotated function element_length
+that takes an interable and returns a list.
+'''
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
