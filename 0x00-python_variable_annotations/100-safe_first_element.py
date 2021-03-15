9ec664e4a1103332cebfd6e55364cb057c596359
Beardocracy <travisjbearden@gmail.com>
2021-03-15 13:35:32 -0500

    Added task 100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from typing import Sequence, Any, Union
+'''
+This module contains a type-annotated function safe_first_element
+that returns the first element of any sequence and returns the
+first element safely.
+'''
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
