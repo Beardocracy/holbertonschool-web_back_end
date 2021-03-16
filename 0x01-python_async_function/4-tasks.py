f140d0154b4aaac187233e9a7610a1944e32ef65
Beardocracy <travisjbearden@gmail.com>
2021-03-16 15:18:36 -0500

    Added type annotations

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,11 +4,12 @@ This module contains the function task_wait_n
 '''
 import random
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Runs task_wait_random n times and returns a sorted list of delay times.
     '''
