9f86b04296007ec3eec73521c0af6ab732a84f6f
Beardocracy <travisjbearden@gmail.com>
2021-03-16 11:21:57 -0500

    Added task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+This module contains the function wait_n
+'''
+import random
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    '''
+    Runs wait_random n times and returns a sorted list of delay times.
+    '''
+    delay_list = []
+    for i in range(n):
+
+        delay_time = await wait_random(max_delay)
+
+        if len(delay_list) == 0:
+            delay_list.append(delay_time)
+        else:
+            for idx in range(len(delay_list)):
+                if delay_time < delay_list[idx]:
+                    delay_list.insert(idx, delay_time)
+                    break
+            if delay_time >= delay_list[len(delay_list) - 1]:
+                delay_list.append(delay_time)
+
+    return delay_list
