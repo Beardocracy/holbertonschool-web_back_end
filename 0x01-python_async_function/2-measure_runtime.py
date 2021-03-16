953b41d28a1f9deeaf4f9fd6edad55e8d68603c4
Beardocracy <travisjbearden@gmail.com>
2021-03-16 15:14:33 -0500

    Added type annotation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     '''
     Meaures the total execution time for wait_n
     '''
