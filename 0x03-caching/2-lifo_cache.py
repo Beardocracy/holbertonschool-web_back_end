9673f2b9603db5a14a7887712724b21b2f816c39
Beardocracy <travisjbearden@gmail.com>
2021-03-22 14:56:27 -0500

    Removed hard coded index

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -7,24 +7,24 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class LIFOCache(BaseCaching):
     '''
-    FIFO Caching system that inherits from BasicCache
+    LIFO Caching system that inherits from BasicCache
     '''
     def __init__(self):
-        ''' FIFO Initialization '''
-        self.queue_list = []
+        ''' LIFO Initialization '''
+        self.stack_list = []
         super().__init__()
 
     def put(self, key, item):
         ''' Assign to the dictionary '''
         if key and item:
             if self.cache_data.get(key):
-                self.queue_list.remove(key)
-            self.queue_list.append(key)
-            self.cache_data[key] = item
-            if len(self.queue_list) > self.MAX_ITEMS:
-                last_item = self.queue_list.pop(3)
+                self.stack_list.remove(key)
+            if len(self.stack_list) >= self.MAX_ITEMS:
+                last_item = self.stack_list.pop()
                 self.cache_data.pop(last_item)
                 print("DISCARD: {}".format(last_item))
+            self.stack_list.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         ''' Returns the value linked to key '''
