dbe4ff0d108004293cb6133c6c694e92623f8ffb
Beardocracy <travisjbearden@gmail.com>
2021-04-01 12:50:05 -0500

    Added method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ This module contains Basic Auth
 '''
 import base64
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,22 @@ class BasicAuth(Auth):
         if not h or not isinstance(h, str) or not h.startswith("Basic "):
             return
         return h[6:]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        ''' Docstring '''
+        if not user_email or not user_pwd:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        try:
+            from models.user import User
+            user = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not user:
+            return None
+        if user[0].is_valid_password(user_pwd):
+            return user[0]
+        else:
+            return None
