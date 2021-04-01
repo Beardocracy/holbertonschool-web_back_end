ca9881d1395853888a828db050e6dc437b4cf409
Beardocracy <travisjbearden@gmail.com>
2021-04-01 12:32:14 -0500

    Added task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 '''
 This module contains Basic Auth
 '''
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -9,3 +10,11 @@ class BasicAuth(Auth):
     ''' Basic Auth '''
     def __init__(self):
         pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        ''' Does what it says '''
+        h = authorization_header
+        if not h or not isinstance(h, str) or not h.startswith("Basic "):
+            return
+        return h[6:]
