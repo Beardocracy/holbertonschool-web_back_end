0e3b5d36bb9fb8cff0da911f84a98b3c487ae60b
Beardocracy <travisjbearden@gmail.com>
2021-04-07 12:46:23 -0500

    Added session cookie method

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ This module contains the Auth class
 '''
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -28,3 +29,8 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         ''' current_user docs '''
         return None
+
+    def session_cookie(self, request=None):
+        ''' Retruns a cookie value from a request '''
+        if request:
+            return request.cookies.get(getenv('SESSION_NAME'))
