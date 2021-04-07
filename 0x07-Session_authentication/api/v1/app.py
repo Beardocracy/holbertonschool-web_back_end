aef551f4c915ca3f9a963fba4a00d4e741fe3d39
Beardocracy <travisjbearden@gmail.com>
2021-04-07 12:52:07 -0500

    Added task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -31,10 +31,12 @@ def before():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth and auth.require_auth(request.path, excluded_paths):
-        if not auth.authorization_header(request):
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
             abort(401)
         if not auth.current_user(request):
             abort(403)
