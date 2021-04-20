7519be8068c032790ba655b120ef55a695b49608
Beardocracy <travisjbearden@gmail.com>
2021-04-20 16:34:12 -0500

    Check task 8

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,8 @@
 import unittest
 from unittest.mock import patch, PropertyMock
 import client
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -53,6 +54,39 @@ class TestGithubOrgClient(unittest.TestCase):
         ''' Docstring docs '''
         goc_obj = client.GithubOrgClient('test')
         self.assertEqual(goc_obj.has_license(repo, license_key), has_license)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    ''' Doc doc docs '''
+    @classmethod
+    def setUpClass(cls):
+        ''' Setup '''
+        config = {
+            'return_value.json.side_effect': [
+                cls.org_payload, cls.repos_payload,
+                cls.org_payload, cls.repos_payload
+            ]
+        }
+        cls.get_patcher = patch('requests.get', **config)
+        cls.mock = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        ''' teardown '''
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        ''' Test public repos '''
+        goc_obj = client.GithubOrgClient('test')
+        self.assertEqual(goc_obj.org, self.org_payload)
+        self.assertEqual(goc_obj.repos_payload, self.repos_payload)
+        self.assertEqual(goc_obj.public_repos(), self.expected_repos)
+        self.assertEqual(goc_obj.public_repos('test'), [])
+        self.mock.assert_called()
 
 
 if __name__ == '__main__':
