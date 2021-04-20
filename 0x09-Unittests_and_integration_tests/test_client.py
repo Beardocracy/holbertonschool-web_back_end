38d0396e82404859abd2440b7e7873fdde8a89d5
Beardocracy <travisjbearden@gmail.com>
2021-04-20 15:57:06 -0500

    Added task 5

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 ''' Client unittests '''
 import unittest
+from unittest.mock import patch, PropertyMock
 import client
 from parameterized import parameterized
-from unittest.mock import patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -14,11 +14,24 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, test_url, test_mock):
-        ''' Docs '''
+        ''' Docs docs '''
         test_mock.return_value = True
         goc_object = client.GithubOrgClient(test_url)
         self.assertEqual(goc_object.org, True)
         test_mock.assert_called_once()
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    def test_public_repos_url(self, test_org):
+        ''' Docstrings '''
+        test_url = 'https://api.github.com/orgs/{}/repos'.format(test_org)
+        test_payload = {'repos_url': test_url}
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=test_payload)):
+            goc_object = client.GithubOrgClient(test_org)
+            self.assertEqual(goc_object._public_repos_url, test_url)
 
 
 if __name__ == '__main__':
