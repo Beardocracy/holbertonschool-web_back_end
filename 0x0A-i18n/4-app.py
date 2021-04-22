8221349f65f953cadfdf0b05d0cc926ced78f272
Beardocracy <travisjbearden@gmail.com>
2021-04-22 13:50:00 -0500

    Added task4

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+''' Basic flask app '''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    ''' Babel Configs '''
+    LANGUAGES = ['en', 'fr']
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+
+babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/', methods=['GET'])
+def welcome():
+    '''
+    GET
+    Return: Render template
+    '''
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    ''' Requests best match for supported languages '''
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
