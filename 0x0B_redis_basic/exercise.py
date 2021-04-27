c921f387e3659d9c7e3f518f177d53a405e4d9f0
Beardocracy <travisjbearden@gmail.com>
2021-04-27 15:29:43 -0500

    Added task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 ''' Redis '''
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -16,3 +16,18 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
+        ''' Get method '''
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, data: bytes) -> str:
+        ''' Get str '''
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        ''' get int '''
+        return int(self._redis.get(data))
