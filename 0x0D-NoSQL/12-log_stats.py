9ebbeb03ba4f22539ec51e34022338e2665f2cd6
Beardocracy <travisjbearden@gmail.com>
2021-05-04 13:09:01 -0500

    Added 12 for testing

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' log stats '''
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    mc = MongoClient('mongodb://localhost:27017').logs.nginx
+    print("{} logs".format(mc.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {}".format(mc.count_documents({"method": "GET"})))
+    print("\tmethod POST: {}".format(mc.count_documents({"method": "POST"})))
+    print("\tmethod PUT: {}".format(mc.count_documents({"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(mc.count_documents({"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(mc.count_documents({"method": "DELETE"})))
+    print("{} status check".format(mc.count_documents({"path": "/status"})))
