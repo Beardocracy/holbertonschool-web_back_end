5eb99d6551cebdac40516a2abc2be8dd913bca17
Beardocracy <travisjbearden@gmail.com>
2021-05-13 20:13:12 +0000

    Added 3

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw TypeError('code must be a String');
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('name must be a String');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('code must be a String');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('name must be a String');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
